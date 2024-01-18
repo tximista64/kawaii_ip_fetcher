a46084600f24e81816a3735b88300df6f9bf9a27
tximista64 <Tximista64@protonmail.com>
2024-01-18 20:36:30 +0100

    add ip geolocalisation

--- kawaii_ip_fetcher.py
+++ kawaii_ip_fetcher.py
@@ -1,18 +1,22 @@
 #!/bin/python3
 import pyfiglet
+import urllib.request
+import requests
+import json
 
 text = "KAWAII IP FETCHER"
 ascii_art = pyfiglet.figlet_format(text)
 print(ascii_art)
 
-
-
-
-import urllib.request
-
 external_ip = urllib.request.urlopen('https://ipv4.icanhazip.com/').read().decode('utf8')
+
 def nl(): print('\n')
 
+request_url = 'https://geolocation-db.com/jsonp/' + external_ip
+response = requests.get(request_url)
+result = response.content.decode()
+result = result.split("(")[1].strip(")")
+result  = json.loads(result)
 
 matrix = "Follow the white rabbit"
 ascii1 = """ (\ /) """
@@ -25,5 +29,8 @@ print(ascii2)
 print(ascii3)
 nl()
 print("Your ip address is : " + format(external_ip))
+print(result)
+
+
 
 
